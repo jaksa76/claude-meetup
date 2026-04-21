7cbd1691bc219a0878777de4211d2a0262baca47
jaksa <jaksa76@gmail.com>
2026-04-21 15:18:32 +0000

    centered hello bar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,7 @@
 export default function App() {
-  return <h1>Hello Bar!</h1>;
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <h2>Hello Bar!</h2>
+    </div>
+  );
 }
