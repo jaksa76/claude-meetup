628537a32d9e017ef9023a1974d757052068b479
jaksa <jaksa76@gmail.com>
2026-04-22 07:25:45 +0000

    serving hello world from server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,17 @@
+import { useEffect, useState } from 'react';
+
 export default function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('/api/hello')
+      .then(r => r.json())
+      .then(data => setMessage(data.message));
+  }, []);
+
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <h2>Hello Bar!</h2>
+      <h2>{message}</h2>
     </div>
   );
 }
